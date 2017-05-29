cfcd85b2d86de68620cb4707d311413bef2ad7ac
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2017-05-29 13:36:30 -0400

    recyclerview content

--- app/src/main/java/com/study/zhenqiangli/mr_basic_circular_reveal_effect/MainActivity.java
+++ app/src/main/java/com/study/zhenqiangli/mr_basic_circular_reveal_effect/MainActivity.java
@@ -1,13 +1,51 @@
 package com.study.zhenqiangli.mr_basic_circular_reveal_effect;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static String title = "Fairy Tale";
+    static String content = "The flowers are flooding the garden with longing and ballads.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv_content);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new RecyclerView.Adapter<ViewHolder>(){
+
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new ViewHolder(getLayoutInflater().inflate(R.layout.list_item, parent, false));
+            }
+
+            @Override
+            public void onBindViewHolder(ViewHolder holder, int position) {
+                holder.titleTextView.setText(title);
+                holder.contentTextView.setText(content);
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+        });
+    }
+
+    private static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView;
+        TextView contentTextView;
+
+        private ViewHolder(View itemView) {
+            super(itemView);
+            titleTextView = (TextView) itemView.findViewById(R.id.title);
+            contentTextView = (TextView) itemView.findViewById((R.id.content));
+        }
     }
 }
