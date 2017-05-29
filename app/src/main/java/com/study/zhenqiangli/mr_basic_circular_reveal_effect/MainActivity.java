c13598bb782cb1f63e348cda9638d03d65434564
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2017-05-29 13:49:09 -0400

    add circular reveal effect

--- app/src/main/java/com/study/zhenqiangli/mr_basic_circular_reveal_effect/MainActivity.java
+++ app/src/main/java/com/study/zhenqiangli/mr_basic_circular_reveal_effect/MainActivity.java
@@ -1,10 +1,12 @@
 package com.study.zhenqiangli.mr_basic_circular_reveal_effect;
 
+import android.animation.Animator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -38,14 +40,32 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private static class ViewHolder extends RecyclerView.ViewHolder {
+    private static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView titleTextView;
         TextView contentTextView;
+
+        boolean flag = false;
 
         private ViewHolder(View itemView) {
             super(itemView);
             titleTextView = (TextView) itemView.findViewById(R.id.title);
             contentTextView = (TextView) itemView.findViewById((R.id.content));
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int finalRadius = (int)Math.hypot(v.getWidth()/2, v.getHeight()/2);
+            if (flag == false) {
+                Animator anim = ViewAnimationUtils
+                        .createCircularReveal(v, v.getWidth()/2, v.getHeight()/2, 0, finalRadius);
+                v.setBackgroundColor(v.getContext().getColor(R.color.green));
+                anim.start();
+                flag = true;
+            } else {
+                v.setBackgroundColor(v.getContext().getColor(R.color.white));
+                flag = false;
+            }
         }
     }
 }
